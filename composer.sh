7932dcae4494b84749a94dbb29e22df732b36f7e
JordiGis <jorgisfer@alu.edu.gva.es>
2024-10-17 19:14:07 +0200

    FIX Initial commit

--- composer.sh
+++ composer.sh
@@ -1,6 +1,6 @@
-docker container exec -it php composer isntall
+docker container exec -it php composer install
 docker container exec -it php composer update
-docker container exec -it php composer slef-update
+docker container exec -it php composer self-update
 docker container exec -it php composer dump-autoload
 
 require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';  
