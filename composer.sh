801d52a6317b3c7a59cc9886f27a80e64540e9e3
JordiGis <jorgisfer@alu.edu.gva.es>
2024-10-17 18:50:31 +0200

    ADD cambios

--- composer.sh
+++ composer.sh
@@ -1,1 +1,6 @@
+docker container exec -it php composer isntall
 docker container exec -it php composer update
+docker container exec -it php composer slef-update
+docker container exec -it php composer dump-autoload
+
+require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';  
